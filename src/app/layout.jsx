a941e1e184b3623f4a1e67f6f958801f4b4de6e3
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-04-18 04:17:59 +0100

    form server actions completed and app layout setup

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+// import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
+import DesignerContextProvider from '@/components/context/DesignerContext';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Form Builder - Create forms and collect responses",
+  description: "Form Builder is a free and open source form builder that lets you create forms and collect responses",
+};
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* <NextTopLoader /> */}
+          <DesignerContextProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
