59cc20b07377497d84de954efb390b24ec99e91a
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-04-21 04:36:57 +0100

    added DesignerElementWrapper styles

--- src/components/DesignerElementWrapper.jsx
+++ src/components/DesignerElementWrapper.jsx
@@ -4,7 +4,11 @@ import { FormElements } from "./FormElements";
 const DesignerElementWrapper = ({ element }) => {
   const DesignerElement = FormElements[element.type]?.designerComponent;
 
-  return <DesignerElement elementInstance={element} />;
+  return (
+    <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none">
+      <DesignerElement elementInstance={element} />
+    </div>
+  );
 };
 
 export default DesignerElementWrapper;
