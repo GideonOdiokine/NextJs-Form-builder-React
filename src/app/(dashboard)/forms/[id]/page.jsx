b183165e240b8351bfec2bf55ea342d4114da5e8
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-08-06 03:21:21 +0100

    setup deployment for vercel and added form page

---
+++ src/app/(dashboard)/forms/[id]/page.jsx
@@ -0,0 +1,17 @@
+import { GetFormById } from '@/actions/form';
+import FormBuilder from '@/components/FormBuilder';
+import React from 'react'
+
+const BuilderPage = async ({params}) => {
+    const {id} = params;
+    // console.log(id);
+    const form = await GetFormById(Number(id));
+
+    if(!form) {
+        throw new Error('Form not found');
+    }
+
+  return <FormBuilder form={form} />
+}
+
+export default BuilderPage
